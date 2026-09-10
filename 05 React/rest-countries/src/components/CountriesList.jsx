573a25b142a64a46a724d4f749caf8f4e502642f
rahul <satyajitsahoo45u@gmail.com>
2026-09-10 11:31:16 +0530

    using rest api country

--- 05 React/rest-countries/src/components/CountriesList.jsx
+++ 05 React/rest-countries/src/components/CountriesList.jsx
@@ -1,12 +1,28 @@
 import countriesData from "../countriesData";
 import CountryCard from "./CountryCard";
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 export default function CountriesList({query ,selectRegion}) {
+  const[countriesData,setCountriesData]=useState([])
   // const[query,setQuery]=useState("")
-  const filteredCountries=countriesData.filter(country=>country.name.common.toLowerCase() .includes(query.toLowerCase())
+  const filteredCountries=countriesData.filter(country=>country.names.common.toLowerCase() .includes(query.toLowerCase())
 && (selectRegion===""||country.region.toLowerCase()===selectRegion.toLowerCase())
 );
   
+useEffect(()=>{
+  fetch(
+  'https://api.restcountries.com/countries/v5?response_fields=names.common,capitals,flag.url_svg,region,population&limit=100',
+  {
+     headers: { 'Authorization': 'Bearer rc_live_3723a4758d154e1882ff8b3e79ec1ea2'
+
+      } }
+)
+.then((response)=>response.json())
+.then((result)=>{
+console.log("result is",result)
+  setCountriesData(result.data.objects)
+})
+},[])
+console.log("//////",countriesData)
   // const handleChange=(e)=>{
     // console.log(e.target.value)
       // setQuery(e.target.value)
@@ -25,8 +41,8 @@ export default function CountriesList({query ,selectRegion}) {
 
           <CountryCard
           key={idx} 
-          flag={country.flags.svg}
-          name={country.name.common}
+          flag={country.flag.url_svg || "www.go0gle.com"}
+          name={country.names.common}
           population={country.population}
           capital={country.capital}
           region={country.region}
